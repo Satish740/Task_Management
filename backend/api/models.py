84d0fb611832cf00983052b3fd8a991c5bc956e2
Satish7490 <jsatish053@gmail.com>
2024-08-20 19:56:35 -0500

    updated backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from django.db import models
-
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
     completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
 
 
 # Create your models here.
